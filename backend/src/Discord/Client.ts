3a711d0fa138ba74582b6f87a6909bfbdd537975
Tolfx <matteus@tolfx.dev>
2022-01-02 18:42:06 +0100

    Discord Client logging in

--- backend/src/Discord/Client.ts
+++ backend/src/Discord/Client.ts
@@ -1,6 +1,12 @@
 import { Client, Intents } from "discord.js";
+import { Discord_Token } from "../Config";
+import CheckerHandler from "./Handlers/CheckHandler";
 
 const allIntents = new Intents(32767);
 export const client = new Client({
     intents: allIntents
 });
+
+CheckerHandler(client);
+
+client.login(Discord_Token);
