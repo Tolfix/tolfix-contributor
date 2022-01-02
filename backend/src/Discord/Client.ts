74c3895bc78e290dcc79babbeced25c87274e66e
Tolfx <matteus@tolfx.dev>
2022-01-02 18:46:24 +0100

    Adding presence on client

--- backend/src/Discord/Client.ts
+++ backend/src/Discord/Client.ts
@@ -7,6 +7,21 @@ export const client = new Client({
     intents: allIntents
 });
 
+client.on("ready", () => {
+
+    client.user?.setPresence({
+        activities: [
+            {
+                name: "Checking for new contributors",
+                type: "WATCHING"
+
+            }
+        ],
+        status: "online"
+    });
+
+});
+
 CheckerHandler(client);
 
 client.login(Discord_Token);
