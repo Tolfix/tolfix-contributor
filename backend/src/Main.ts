3e92f9864a7719d5190f95b9157ce721bf3d5f8f
Tolfx <matteus@tolfx.dev>
2022-01-03 01:37:50 +0100

    Modified < Main >

--- backend/src/Main.ts
+++ backend/src/Main.ts
@@ -1,5 +1,10 @@
 require("dotenv").config();
+// Hate this .default shit....
+require("./Cron/Github/CronRespositories").default(true);
 import "./Database/Mongo";
 import "./Server/Express";
 import "./Discord/Client";
 import "./Events/NodeEvents";
+
+// Crons
+import "./Cron/PointGathering/CronPointGathering";
