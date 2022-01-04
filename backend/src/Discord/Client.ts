317745d943f2638f1dbb21635a1925eec4c21c70
Tolfx <matteus@tolfx.dev>
2022-01-04 20:10:46 +0100

    Modified < Discord/Client >

--- backend/src/Discord/Client.ts
+++ backend/src/Discord/Client.ts
@@ -1,12 +1,24 @@
-import { Client, Intents } from "discord.js";
+import { Client, Collection, Intents } from "discord.js";
 import { Discord_Token } from "../Config";
 import Logger from "../Lib/Logger";
 import CheckerHandler from "./Handlers/CheckHandler";
+import CommandHandler from "./Handlers/CommandHandler";
+import Command from "./Struct/Command";
 
 const allIntents = new Intents(32767);
 export const client = new Client({
     intents: allIntents
 });
+
+declare module 'discord.js' 
+{
+    export interface Client {
+      commands: Collection<string, Command>;
+      category: string[];
+    }
+};
+
+client.commands = new Collection();
 
 client.on("ready", () => {
     Logger.info(`Client ready.`)
@@ -24,5 +36,30 @@ client.on("ready", () => {
 });
 
 CheckerHandler(client);
+CommandHandler(client);
+
+const Prefix = "cb!"
+client.on("messageCreate", (message) => {
+
+    if (message.author.bot) return;
+    if (!message.guild) return;
+
+    const args = message.content.slice(Prefix.length).trim().split(/ +/g);
+    const cmd: any = args.shift()?.toLowerCase();
+
+    if (cmd.length === 0) return;
+    
+    let command = (client.commands.get(cmd))?.run;
+
+    if (command)
+    {
+        Logger.info(`Command ${cmd} was called by ${message.author.username}`);
+        command(
+            client,
+            message,
+            args
+        );
+    };
+});
 
 client.login(Discord_Token);
